9e1b88c333719a203b204af1960a596b63f44f64
Rimon R Pallan <rimon.x.pallan.-nd@disney.com>
2021-04-09 14:02:12 +0530

    Added Ui for SignIn Page and navigate to Home page

--- app/src/main/java/com/promatas/wiseli/ui/signInPage/SignInFragment.java
+++ app/src/main/java/com/promatas/wiseli/ui/signInPage/SignInFragment.java
@@ -1,14 +1,18 @@
 package com.promatas.wiseli.ui.signInPage;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.Fragment;
+
+import com.promatas.wiseli.MainActivity;
 import com.promatas.wiseli.R;
 
 /**
@@ -16,7 +20,7 @@ import com.promatas.wiseli.R;
  */
 public class SignInFragment extends Fragment {
 
-
+    private AppCompatButton btnLogin;
     public SignInFragment() {
         // Required empty public constructor
     }
@@ -29,4 +33,13 @@ public class SignInFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_sign_in, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnLogin = view.findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), MainActivity.class));
+            getActivity().finish();
+        });
+    }
 }
