2c376256954a4d5c54f60675fec2e5d840260947
Rimon R Pallan <rimon.x.pallan.-nd@disney.com>
2021-04-11 10:02:33 +0530

    Navigation Drawer for and its fragments

--- app/src/main/java/com/promatas/wiseli/ui/activity/LandingActivity.java
+++ app/src/main/java/com/promatas/wiseli/ui/activity/LandingActivity.java
@@ -4,22 +4,33 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.NavGraph;
 import androidx.navigation.NavInflater;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.navigation.NavigationView;
 import com.promatas.wiseli.R;
 
 public class LandingActivity extends AppCompatActivity {
 
     public NavGraph navGraph;
     public NavController navController;
+    public DrawerLayout drawerLayout;
+    public NavigationView navigationView;
+    public Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
 
         NavHostFragment navHostFragment =
                 (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.landing_nav_host_fragment);
@@ -27,5 +38,20 @@ public class LandingActivity extends AppCompatActivity {
         navGraph = navInflater.inflate(R.navigation.landing_navigation);
         navController = navHostFragment.getNavController();
         navGraph.setStartDestination(R.id.landingFragment);
+
+        setupDrawerLayout();
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+    }
+
+    private void setupDrawerLayout() {
+        // Set up ActionBar
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, drawerLayout);
     }
 }
---
+++ app/src/main/java/com/promatas/wiseli/ui/fragment/groupCircles/GroupCirclesFragment.java
@@ -0,0 +1,31 @@
+package com.promatas.wiseli.ui.fragment.groupCircles;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.promatas.wiseli.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GroupCirclesFragment extends Fragment {
+
+
+    public GroupCirclesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_group_circles, container, false);
+    }
+
+}
