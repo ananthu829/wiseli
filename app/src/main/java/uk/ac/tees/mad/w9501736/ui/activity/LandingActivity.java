837c60c7daab589a43216e7dfa2c0d13557e4e1e
ananthu829 <ananthuviswanath829@gmail.com>
2021-04-29 19:20:35 +0530

    navheader header  text updated

--- app/src/main/java/uk/ac/tees/mad/w9501736/ui/activity/LandingActivity.java
+++ app/src/main/java/uk/ac/tees/mad/w9501736/ui/activity/LandingActivity.java
@@ -1,6 +1,9 @@
 package uk.ac.tees.mad.w9501736.ui.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
@@ -11,11 +14,13 @@ import androidx.navigation.NavInflater;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.navigation.NavigationView;
 
 import uk.ac.tees.mad.w9501736.R;
 import uk.ac.tees.mad.w9501736.data.model.WiseLiUser;
 import uk.ac.tees.mad.w9501736.ui.BaseActivity;
+import uk.ac.tees.mad.w9501736.utils.AppConstants;
 import uk.ac.tees.mad.w9501736.utils.AppPreferences;
 
 public class LandingActivity extends BaseActivity {
@@ -46,10 +51,18 @@ public class LandingActivity extends BaseActivity {
 
         setupDrawerLayout();
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
-
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = (TextView) headerView.findViewById(R.id.navUsername);
+        TextView navEmail = (TextView) headerView.findViewById(R.id.navEmail);
+        ImageView navProfle =(ImageView)headerView.findViewById(R.id.navProfile) ;
 
         mAppPreferences = AppPreferences.getInstance(this);
         userDetails = mAppPreferences.getUserCashedInfo();
+
+
+        navUsername.setText(userDetails.getUsername());
+        navEmail.setText(userDetails.getEmail());
+        Glide.with(this).load(AppConstants.API_BASE_URL+userDetails.getProfilePic()).into(navProfle);
     }
 
     private void setupDrawerLayout() {
