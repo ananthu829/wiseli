22b12e43b30ea591d3f3283655622f0680998df8
ananthu829 <ananthuviswanath829@gmail.com>
2021-04-30 20:40:42 +0530

    Find Friends UI created

--- app/src/main/java/uk/ac/tees/mad/w9501736/ui/fragment/groupCircles/FindNewUserFragment.java
+++ app/src/main/java/uk/ac/tees/mad/w9501736/ui/fragment/groupCircles/FindNewUserFragment.java
@@ -1,18 +1,20 @@
 package uk.ac.tees.mad.w9501736.ui.fragment.groupCircles;
 
-
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.widget.Toast;
+
 import java.util.ArrayList;
+
 import butterknife.BindView;
 import uk.ac.tees.mad.w9501736.R;
 import uk.ac.tees.mad.w9501736.adapters.UserListAdapter;
@@ -20,35 +22,22 @@ import uk.ac.tees.mad.w9501736.models.AvailableUserList;
 import uk.ac.tees.mad.w9501736.ui.BaseFragment;
 import uk.ac.tees.mad.w9501736.ui.helper.AdapterInterface;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class GroupCirclesFragment extends BaseFragment implements AdapterInterface {
 
-    @BindView(R.id.fab)
-    FloatingActionButton fab;
+public class FindNewUserFragment extends BaseFragment implements AdapterInterface {
+
 
     @BindView(R.id.rvUser)
     RecyclerView rvUser;
 
+    @BindView(R.id.rlSearchView)
+    SearchView searchView;
+
     AdapterInterface adapterInterface;
-
-
-    public GroupCirclesFragment() {
-        // Required empty public constructor
-    }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_group_circles, container, false);
-    }
-
-
-    @Override
-    protected int layoutRes() {
-        return R.layout.fragment_group_circles;
+        return inflater.inflate(R.layout.fragment_find_new_user, container, false);
     }
 
     @Override
@@ -63,13 +52,34 @@ public class GroupCirclesFragment extends BaseFragment implements AdapterInterfa
         data.add(new AvailableUserList("User 4", false));
         rvUser.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
         rvUser.setAdapter(new UserListAdapter(data, adapterInterface));
-        btnClick(view);
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                if(data.contains(query)){
+                    //adapter.getFilter().filter(query);
+                }else{
+                    Toast.makeText(getContext(), "No Match found",Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //    adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
     }
 
-    public void btnClick(View view) {
-        fab.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_groupCirclesFragment_to_findNewUserFragment));
-    }
 
+
+    @Override
+    protected int layoutRes() {
+        return R.layout.fragment_find_new_user;
+    }
 
     @Override
     public void onItemClicked(String title) {
